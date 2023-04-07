7000206975840deac312571693e0ce6f1421ef8d
Kaden Kapadia <106453833+kkapadia10@users.noreply.github.com>
2023-04-06 22:31:34 -0700

    Adding the extra credit questions.

---
+++ extra-1.sql
@@ -0,0 +1,1 @@
+SELECT age FROM persons WHERE (gender = 'Female' AND title = 'Dr' AND city = 'West Palm Beach' ') OR (gender = 'Male' AND title = 'Rev' AND city = 'Phoenix' ');
---
+++ extra-3.sql
@@ -0,0 +1,1 @@
+SELECT MAX(age) AS 'maximum_age', MIN(age) AS 'minimum_age', AVG(age) AS 'average_age' FROM persons;
