0a91343b1ee53f120655e1b0c41fad5e2b767f38
Kaden Kapadia <106453833+kkapadia10@users.noreply.github.com>
2023-04-06 22:55:59 -0700

    Updating questions

--- question-9.sql
+++ question-9.sql
@@ -1,1 +1,1 @@
-DELETE FROM persons WHERE city = 'Kansas City' OR city = 'Pittsburgh' OR city = 'Jacksonville'
+DELETE FROM persons WHERE city = 'Kansas City' OR city = 'Pittsburgh' OR city = 'Jacksonville';
